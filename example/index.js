91c5be3c0b625a3474a740fdd000ffdccddb20cd
tianxiangbing <tianxiangbing@erayt.com>
2018-05-08 20:45:10 +0800

    '时间格式化返回'

--- example/index.js
+++ example/index.js
@@ -1,15 +1,15 @@
 const CountDown = require('../src/index');
-CountDown.init({date:'2018/05/08 16:23',callback:()=>{
+CountDown.init({date:'2018/05/08 21:23',callback:()=>{
     console.log('第一个ok')
 },update:(count)=>{
-    console.log('第一个还剩:'+count +'s')
-}}).init({date:'2018/05/08 16:24',callback:()=>{
+    console.log('第一个还剩:'+count )
+}}).init({date:'2018/05/09 22:24',callback:()=>{
     console.log('第二个ok')
 },update:(count)=>{
-    console.log('第二个还剩:'+count +'s')
+    console.log('第二个还剩:'+count )
 }})
 CountDown.init({date:20,callback:()=>{
     console.log('第三个ok')
 },update:(count)=>{
-    console.log('第三个还剩:'+count +'s')
+    console.log('第三个还剩:'+count )
 }});
--- src/index.js
+++ src/index.js
@@ -22,63 +22,85 @@
 })(function () {
     "use strict";
     const CountDown = {
-        timer:null,
-        arr:[],
+        timer: null,
+        arr: [],
         //这里是否可以考虑传进来系统的当前日期作为基准值.
         init({ date, callback = () => { }, update = () => { } }) {
             //对传递的时间作兼容处理，可接收时间对象和时间秒
-            if(typeof date ==='object'){
+            if (typeof date === 'object') {
                 //时间对象
                 date = date.getTime();
-            }else
-            if(typeof date ==='number'){
-                if(String(date).length >8){
-                    date  = new Date(date);
-                }else{
-                    let now  = +new Date();
-                    date = now + date*1000;
-                    console.log(date,111)
+            } else
+                if (typeof date === 'number') {
+                    if (String(date).length > 8) {
+                        date = new Date(date);
+                    } else {
+                        let now = +new Date();
+                        date = now + date * 1000;
+                        console.log(date, 111)
+                    }
+                } else if (typeof date === 'string') {
+                    let d = new Date(date);
+                    date = d.getTime()
                 }
-            }else if(typeof date ==='string') {
-                let d = new Date(date);
-                date = d.getTime()
-            }
             let obj = {
                 date,
                 callback,
                 update
             };
             this.arr.push(obj);
-            if(!this.timer){
+            if (!this.timer) {
                 // this.update();
-                this.timer = setInterval(()=>{
+                this.timer = setInterval(() => {
                     this.update();
-                },1000)
+                }, 1000)
             }
             return this;
         }
-        ,update(){
+        , update() {
             let now = new Date();
-            let second = Math.ceil(now.getTime()/1000);
+            let second = Math.ceil(now.getTime() / 1000);
             // console.log('second',second);
-            let timer = setTimeout(()=>{
-                this.arr.forEach((item,idx)=>{
-                    let targetSec = Math.ceil(item.date/1000) ;
-                    if(second>= targetSec){
+            let timer = setTimeout(() => {
+                this.arr.forEach((item, idx) => {
+                    let targetSec = Math.ceil(item.date / 1000);
+                    if (second >= targetSec) {
                         //到时
                         item.callback();
                         this.arr.splice(idx, 1);
-                        if(this.arr.length === 0){
+                        if (this.arr.length === 0) {
                             // console.log('over')
                             clearInterval(this.timer);
                         }
-                    }else{
+                    } else {
                         // console.log(targetSec,second)
-                        item.update(targetSec - second);
+                        let ms = targetSec - second;
+                        item.update(this.formatTime(ms * 1000));
                     }
                 });
                 clearTimeout(timer);
-            },0)
+            }, 0)
+        },
+        /* 
+        * 毫秒转化时分秒毫秒 
+        */
+        formatTime(mss) {
+            if (mss <= 0) {
+                return '00';
+            }
+            var days = parseInt(mss / (1000 * 60 * 60 * 24));
+            var hours = parseInt((mss % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = parseInt((mss % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = (mss % (1000 * 60)) / 1000;
+            let returnValue = '';
+            if (days > 0) {
+                returnValue += days + "天";
+            }
+            if (hours > 0) {
+                returnValue += hours + "小时";
+            }
+            returnValue += (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
+            return returnValue;
         }
     }
     return CountDown;
